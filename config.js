ab31f21556b9ae96860ddd7aa61d10b1475309f4
demon241 <laidanezaidane@gmail.com>
2025-01-12 00:58:21 +0100

    Update config.jsp
    Signed-off-by: demon241 <laidanezaidane@gmail.com>

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "DAgzERLT#4iUVpagAJFdZK59bc1V6NA9rGxN2P7g2hdmAx6fO-EY",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
